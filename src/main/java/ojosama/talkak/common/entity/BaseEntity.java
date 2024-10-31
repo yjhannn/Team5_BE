c75b2c4e2f2ab44769e1a1decb6bd9f5b2ef3856
Hyewon Seok <hws2008@naver.com>
2024-10-31 23:59:17 +0900

    Refactor: base entity 추가

---
+++ src/main/java/ojosama/talkak/common/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package ojosama.talkak.common.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
--- src/main/java/ojosama/talkak/viewingRecord/domain/ViewingRecord.java
+++ src/main/java/ojosama/talkak/viewingRecord/domain/ViewingRecord.java
@@ -2,24 +2,18 @@ package ojosama.talkak.viewingRecord.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import ojosama.talkak.common.entity.BaseEntity;
 import ojosama.talkak.member.domain.Member;
 import ojosama.talkak.video.domain.Video;
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "viewing_records")
-public class ViewingRecord {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class ViewingRecord extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "member_id", nullable = false)
