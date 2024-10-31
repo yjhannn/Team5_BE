bd4383bc7905ac83aa290fa14047bf04da78d297
anacnoda77 <sm1108shin@naver.com>
2024-11-01 02:33:05 +0900

    Feat: Reactions 좋아요 토글 메서드 구현

--- src/main/java/ojosama/talkak/reaction/service/ReactionService.java
+++ src/main/java/ojosama/talkak/reaction/service/ReactionService.java
@@ -2,17 +2,14 @@ package ojosama.talkak.reaction.service;
 
 import ojosama.talkak.common.exception.TalKakException;
 import ojosama.talkak.common.exception.code.ReactionError;
-import ojosama.talkak.member.domain.Member;
 import ojosama.talkak.member.repository.MemberRepository;
-import ojosama.talkak.reaction.domain.Reaction;
-import ojosama.talkak.reaction.domain.ReactionId;
-import ojosama.talkak.reaction.repository.ReactionRepository;
 import ojosama.talkak.redis.HashConverter;
 import ojosama.talkak.redis.RedisService;
+import ojosama.talkak.redis.domain.Reactions;
 import ojosama.talkak.redis.domain.VideoInfo;
 import ojosama.talkak.redis.innerkey.VideoHashKey;
 import ojosama.talkak.redis.key.VideoKey;
-import ojosama.talkak.video.domain.Video;
+import ojosama.talkak.redis.repository.ReactionsRepository;
 import ojosama.talkak.video.repository.VideoRepository;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -20,17 +17,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReactionService {
 
-    private final ReactionRepository reactionRepository;
+    private final ReactionsRepository reactionsRepository;
     private final MemberRepository memberRepository;
     private final VideoRepository videoRepository;
     private final RedisService redisService;
     private final RedisTemplate<String, Object> redisTemplate;
     private final HashConverter hashConverter;
 
-    public ReactionService(ReactionRepository reactionRepository, MemberRepository memberRepository,
-        VideoRepository videoRepository, RedisService redisService, RedisTemplate<String, Object> redisTemplate,
+    public ReactionService(ReactionsRepository reactionsRepository,
+        MemberRepository memberRepository,
+        VideoRepository videoRepository, RedisService redisService,
+        RedisTemplate<String, Object> redisTemplate,
         HashConverter hashConverter) {
-        this.reactionRepository = reactionRepository;
+        this.reactionsRepository = reactionsRepository;
         this.memberRepository = memberRepository;
         this.videoRepository = videoRepository;
         this.redisService = redisService;
@@ -38,20 +37,20 @@ public class ReactionService {
         this.hashConverter = hashConverter;
     }
 
-    public void toggleLike(Long videoId, Long memberId) {
-        Member member = memberRepository.findById(memberId)
-            .orElseThrow(() -> TalKakException.of(ReactionError.INVALID_MEMBER_ID));
-        Video video = videoRepository.findById(videoId)
-            .orElseThrow(() -> TalKakException.of(ReactionError.INVALID_VIDEO_ID));
+    // 영상을 시청함과 동시에 Reactions 생성
+    public Reactions createReactions(Long memberId, Long videoId) {
+        return reactionsRepository.save(memberId, videoId, Reactions.createReaction());
+    }
 
-        ReactionId reactionId = new ReactionId(memberId, videoId);
-        reactionRepository.findById(reactionId)
-            .ifPresentOrElse(
-                existingReaction -> removeExistingReactionAndUpdateLikes(existingReaction, video),
-                () -> {
-                    Reaction newReaction = new Reaction(reactionId, member, video, true);
-                    reactionRepository.save(newReaction);
-                    video.incrementLikes();
+    public void toggleLike(Long memberId, Long videoId) {
+        reactionsRepository.findByMemberIdAndVideoId(memberId, videoId)
+            .ifPresentOrElse(reactions -> {
+                    reactions.updateLike();
+                    reactionsRepository.save(memberId, videoId, reactions);
+                }, ()
+                    -> {
+                    createReactions(memberId, videoId);
+                    throw TalKakException.of(ReactionError.FAILED_PROCESS_REQUEST);
                 }
             );
     }
@@ -68,9 +67,4 @@ public class ReactionService {
         return hashConverter.FromMap(redisService.getHashOps(key), VideoInfo.class);
     }
 
-
-    private void removeExistingReactionAndUpdateLikes(Reaction existingReaction, Video video) {
-        reactionRepository.delete(existingReaction);
-        video.decrementLikes();
-    }
 }
