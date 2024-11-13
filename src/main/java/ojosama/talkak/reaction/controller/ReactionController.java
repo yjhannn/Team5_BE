abc3a91d532425dd0f67a27a375432b46400a3f6
anacnoda77 <sm1108shin@naver.com>
2024-11-14 02:03:52 +0900

    Fix: 필터 미적용 대상 uri 오류 해결 및 Comment 스웨거 추가

--- src/main/java/ojosama/talkak/reaction/controller/ReactionController.java
+++ src/main/java/ojosama/talkak/reaction/controller/ReactionController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import ojosama.talkak.reaction.service.ReactionService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -14,15 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/videos/{videoId}/reactions")
 @RequiredArgsConstructor
-public class ReactionController {
+public class ReactionController implements ReactionApiController {
 
     private final ReactionService reactionService;
 
     @PostMapping("/like")
     public ResponseEntity<Void> toggleLike(
-        @PathVariable Long videoId,
-        @RequestHeader("memberId") Long memberId) {
-        reactionService.toggleLike(videoId, memberId);
+        @PathVariable Long videoId, Authentication authentication) {
+        Long id = Long.valueOf(authentication.getPrincipal().toString());
+        reactionService.toggleLike(videoId, id);
         return ResponseEntity.ok().build();
     }
 }
