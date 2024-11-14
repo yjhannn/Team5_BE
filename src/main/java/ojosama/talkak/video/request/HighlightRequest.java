ad6f8c7ec4ba50e4c4f407da06e7574c476c244b
xkrwjdals <developjmt@gmail.com>
2024-11-14 21:48:35 +0900

    Refactor: pythonDto 대신 title, categoryId, memberId 값을 받도록 변경

--- src/main/java/ojosama/talkak/video/request/HighlightRequest.java
+++ src/main/java/ojosama/talkak/video/request/HighlightRequest.java
@@ -1,5 +1,5 @@
 package ojosama.talkak.video.request;
 
-public record HighlightRequest(Integer index, String s3Url, PythonDto pythonDto) {
+public record HighlightRequest(Integer index, String s3Url, String title, Long categoryId, Long memberId) {
 
 }
